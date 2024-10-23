fc9d7d32e55b024d4c145dd953bedef1dec5261d
bmk2010 <profiy70@gmail.com>
2024-10-23 20:56:23 +0500

    fix

--- src/app/api/hello/route.js
+++ src/app/api/hello/route.js
@@ -1,4 +1,4 @@
-export async function GET(request) {
+export async function GET() {
     return new Response(JSON.stringify({ message: 'Hello, Next.js 15 API!' }), {
       status: 200,
       headers: { 'Content-Type': 'application/json' },
